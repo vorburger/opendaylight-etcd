170310c4d484fbafae36a835c5a9dd7a2ef59149
Michael Vorburger <vorburger@redhat.com>
2017-10-25 19:17:29 +0200

    EtcdDBTest (failing) like AbstractDataBrokerTestTest
    currently fails with UnsupportedOperationException from EtcdDataStore,
    which now will have to be implemented, to get that test to pass... ;)
    
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- testutils/src/test/java/org/opendaylight/etcd/testutils/test/EtcdDBTest.java
+++ testutils/src/test/java/org/opendaylight/etcd/testutils/test/EtcdDBTest.java
@@ -8,10 +8,24 @@
 package org.opendaylight.etcd.testutils.test;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.opendaylight.controller.md.sal.test.model.util.ListsBindingUtils.TOP_FOO_KEY;
+import static org.opendaylight.controller.md.sal.test.model.util.ListsBindingUtils.path;
+import static org.opendaylight.controller.md.sal.test.model.util.ListsBindingUtils.topLevelList;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.etcd.testutils.TestEtcdDataBrokersProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.augment.rev140709.TreeComplexUsesAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.augment.rev140709.TreeComplexUsesAugmentBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.augment.rev140709.complex.from.grouping.ContainerWithUsesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.list.rev140701.Top;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.md.sal.test.list.rev140701.TopBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
  * Tests the etcd-based data broker.
@@ -20,17 +34,48 @@ import org.opendaylight.etcd.testutils.TestEtcdDataBrokersProvider;
  */
 public class EtcdDBTest {
 
-//    private static DataBroker dataBroker;
+    private static final InstanceIdentifier<Top> TOP_PATH = InstanceIdentifier.create(Top.class);
 
-//    @BeforeClass
-//    public static void beforeClass() {
-//        dataBroker = new TestEtcdDataBrokersProvider().getDataBroker();
-//    }
+    private static DataBroker dataBroker;
+
+    @BeforeClass
+    public static void beforeClass() {
+        dataBroker = new TestEtcdDataBrokersProvider().getDataBroker();
+    }
 
     @Test
     public void testSetUp() {
-        DataBroker dataBroker = new TestEtcdDataBrokersProvider().getDataBroker();
         assertThat(dataBroker).isNotNull();
     }
+
+    // as in org.opendaylight.controller.md.sal.binding.test.tests.AbstractDataBrokerTestTest
+
+    @Test
+    public void bPutSomethingIntoDataStore() throws Exception {
+        writeInitialState();
+        assertThat(isTopInDataStore()).isTrue();
+    }
+
+    @Test
+    public void cEnsureDataStoreIsEmptyAgainInNewTest() throws ReadFailedException {
+        assertThat(isTopInDataStore()).isFalse();
+    }
+
+    private void writeInitialState() throws Exception {
+        WriteTransaction initialTx = dataBroker.newWriteOnlyTransaction();
+        initialTx.put(LogicalDatastoreType.OPERATIONAL, TOP_PATH, new TopBuilder().build());
+        TreeComplexUsesAugment fooAugment = new TreeComplexUsesAugmentBuilder()
+                .setContainerWithUses(new ContainerWithUsesBuilder().setLeafFromGrouping("foo").build()).build();
+        initialTx.put(LogicalDatastoreType.OPERATIONAL, path(TOP_FOO_KEY), topLevelList(TOP_FOO_KEY, fooAugment));
+        initialTx.submit().get();
+    }
+
+    private boolean isTopInDataStore() throws ReadFailedException {
+        try (ReadOnlyTransaction readTx = dataBroker.newReadOnlyTransaction()) {
+            return readTx.read(LogicalDatastoreType.OPERATIONAL, TOP_PATH).checkedGet().isPresent();
+        }
+    }
+
+    // TODO as in org.opendaylight.controller.md.sal.dom.broker.impl.DOMBrokerTest & Co.
 
 }
