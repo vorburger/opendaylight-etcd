7e870ee1d4fb3af52848136e2be17cba5b25d882
Michael Vorburger <vorburger@redhat.com>
2017-10-25 19:05:43 +0200

    testutils with TestEtcdDataBrokersProvider etc.
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ testutils/src/main/java/org/opendaylight/etcd/testutils/SchemaContextSingleton.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.etcd.testutils;
+
+import java.util.function.Supplier;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+
+/**
+ * {@link SchemaContext} singleton holder (static).
+ *
+ * <p>This is useful in scenarios such as unit tests, but not OSGi environments,
+ * where there is a flat classpath and thus really only one single
+ * SchemaContext.
+ *
+ * @author Michael Vorburger.ch
+ */
+public final class SchemaContextSingleton {
+
+    private static SchemaContext staticSchemaContext;
+
+    public static synchronized SchemaContext getSchemaContext(Supplier<SchemaContext> supplier) {
+        if (staticSchemaContext == null) {
+            staticSchemaContext = supplier.get();
+        }
+        return staticSchemaContext;
+    }
+
+    private SchemaContextSingleton() { }
+
+}
