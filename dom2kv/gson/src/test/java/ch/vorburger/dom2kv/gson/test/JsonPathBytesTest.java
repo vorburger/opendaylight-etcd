615cf3ac7735451d4d5ba9a27b7f2f0f0ece0a4d
Michael Vorburger <vorburger@redhat.com>
2018-06-30 01:18:41 +0200

    initial version of JsonPathBytes (but this is platform specific)
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ dom2kv/gson/src/test/java/ch/vorburger/dom2kv/gson/test/JsonPathBytesTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package ch.vorburger.dom2kv.gson.test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import ch.vorburger.dom2kv.Sequence;
+import ch.vorburger.dom2kv.gson.jetcd.JsonPathBytes;
+import ch.vorburger.dom2kv.impl.SequenceListImpl;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link JsonPathBytes}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class JsonPathBytesTest {
+
+    @Test
+    public void stringSeqToBytes() {
+        Sequence<String> seq = JsonPathBytes.BYTES_TO_STRING_SEQ
+            .apply(JsonPathBytes.STRING_SEQ_TO_BYTES.apply(new SequenceListImpl<>("a", "b", "⛑️")));
+        assertThat(seq.head()).isEqualTo("a");
+        assertThat(seq.tail().head()).isEqualTo("b");
+        assertThat(seq.tail().tail().head()).isEqualTo("⛑️");
+    }
+}
