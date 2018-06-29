e09da28e665f38c605a63815b76393e748cb081c
Michael Vorburger <vorburger@redhat.com>
2018-06-29 06:35:46 +0200

    dom2kv: move ByteSeq out of the way into new package "bite" ;)
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- dom2kv/dom2kv/src/main/java/ch/vorburger/dom2kv/bite/ByteSeq.java
+++ dom2kv/dom2kv/src/main/java/ch/vorburger/dom2kv/bite/ByteSeq.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package ch.vorburger.dom2kv;
+package ch.vorburger.dom2kv.bite;
 
 import com.google.errorprone.annotations.Var;
 import java.util.Arrays;
