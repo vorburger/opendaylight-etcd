21543a64c66e758a0bc244ed0fbe2207b174a5cc
Michael Vorburger <vorburger@redhat.com>
2018-09-05 00:34:59 +0200

    fix org.opendaylight.mdsal.dom.api.DOMDataBroker OSGi service
    Now just missing these:
    
    * org.opendaylight.controller.md.sal.dom.api.DOMDataBroker
    
    * org.opendaylight.controller.md.sal.dom.api.DOMRpcService
    * org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService
    
    * org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipService
    
    * org.opendaylight.controller.md.sal.dom.api.DOMNotificationService
    * org.opendaylight.controller.md.sal.dom.api.DOMNotificationPublishService
    * org.opendaylight.controller.md.sal.dom.spi.DOMNotificationSubscriptionListenerRegistry
    
    
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- ds/src/main/java/org/opendaylight/etcd/ds/impl/OsgiDOMDataBrokerService.java
+++ ds/src/main/java/org/opendaylight/etcd/ds/impl/OsgiDOMDataBrokerService.java
@@ -15,7 +15,6 @@ import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.mdsal.dom.spi.ForwardingDOMDataBroker;
 import org.ops4j.pax.cdi.api.OsgiService;
-import org.ops4j.pax.cdi.api.OsgiServiceProvider;
 
 /**
  * {@link DOMDataBroker} registered in the OSGi service registry.
@@ -23,12 +22,20 @@ import org.ops4j.pax.cdi.api.OsgiServiceProvider;
  * @author Michael Vorburger.ch
  */
 @Singleton
-@OsgiServiceProvider(classes = DOMDataBroker.class)
+// do NOT @OsgiServiceProvider(classes = DOMDataBroker.class), because we need odl:type="default" so BP XML
 public class OsgiDOMDataBrokerService extends ForwardingDOMDataBroker {
 
     private final EtcdDOMDataBrokerProvider wiring;
 
     @Inject
+    public OsgiDOMDataBrokerService(@OsgiService DOMSchemaService schemaService) throws Exception {
+        // TODO Remove this constructor with hard-coded etcd server endpoint by using @OsgiService Client (below)
+        // For the moment this does not work, most probably because of the org/opendaylight/blueprint/ds-blueprint.xml
+        // which we need so that we can look up other OSGi services which mdsal and controller registered; this should
+        // get solved with odlparent 4.0.0 which does away with org/opendaylight/blueprint and the 2 (std VS odl) BPs.
+        this(schemaService, Client.builder().endpoints("http://localhost:2379").build());
+    }
+
     // the Client is set up in the OSGi Service registry by io.etcd:jetcd-osgi, based on etc/io.etcd.jetcd.cfg
     public OsgiDOMDataBrokerService(@OsgiService DOMSchemaService schemaService, @OsgiService Client etcdClient)
             throws Exception {
