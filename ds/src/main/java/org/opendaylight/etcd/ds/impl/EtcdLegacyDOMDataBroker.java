cb1b038542041923c68459561285c1120b6db851
Michael Vorburger <vorburger@redhat.com>
2018-09-05 01:09:56 +0200

    fix missing org.opendaylight.controller.md.sal.dom.api.DOMDataBroker
    Now missing
    org.opendaylight.mdsal.eos.dom.api.DOMEntityOwnershipService, only?
    Unclear why DOMRpcService* and DOMNotification* no longer shows up..

---
+++ ds/src/main/java/org/opendaylight/etcd/ds/impl/EtcdLegacyDOMDataBroker.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.etcd.ds.impl;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.sal.core.compat.LegacyDOMDataBrokerAdapter;
+
+/**
+ * Adapter between the legacy controller API-based DOMDataBroker and the mdsal API-based DOMDataBroker.
+ * This class's only purpose is to cause the Import-Package: for LegacyDOMDataBrokerAdapter
+ * and (controller's) DOMDataBroker in the MANIFEST.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Singleton
+@SuppressWarnings("deprecation") // because that's the whole point of this class
+// the implements, while implicit to Java, is required for an Import-Package: for controller
+@SuppressFBWarnings("RI_REDUNDANT_INTERFACES")
+public class EtcdLegacyDOMDataBroker extends LegacyDOMDataBrokerAdapter
+        implements org.opendaylight.controller.md.sal.dom.api.DOMDataBroker {
+
+    @Inject
+    public EtcdLegacyDOMDataBroker(OsgiDOMDataBrokerService delegate) {
+        super(delegate);
+    }
+
+}
