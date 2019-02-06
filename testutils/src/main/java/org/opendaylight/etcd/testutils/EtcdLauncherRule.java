1869f817ff8e3015b515dd207a764983cdd2a59c
Michael Vorburger <vorburger@redhat.com>
2019-02-06 20:23:25 +0100

    introduce new EtcdLauncherRule

---
+++ testutils/src/main/java/org/opendaylight/etcd/testutils/EtcdLauncherRule.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.etcd.testutils;
+
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Collections;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.opendaylight.etcd.launcher.EtcdLauncher;
+
+/**
+ * JUnit Rule for {@link EtcdLauncher}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class EtcdLauncherRule implements TestRule {
+
+    // TODO upstream this into jetcd ...
+
+    protected final Path etcdWorkingDirectory;
+    protected EtcdLauncher currentEtcdServer;
+
+    public EtcdLauncherRule() {
+        this(Paths.get("target/etcd"));
+    }
+
+    public EtcdLauncherRule(Path etcdWorkingDirectory) {
+        this.etcdWorkingDirectory = etcdWorkingDirectory;
+    }
+
+    @Override
+    public Statement apply(Statement statement, Description description) {
+        return new Statement() {
+
+            @Override
+            public void evaluate() throws Throwable {
+                try (EtcdLauncher etcdServer = new EtcdLauncher(etcdWorkingDirectory, true)) {
+                    etcdServer.start();
+                    currentEtcdServer = etcdServer;
+
+                    statement.evaluate();
+                }
+                currentEtcdServer = null;
+            }
+        };
+    }
+
+    public Collection<URI> getClusterURIs() {
+        if (currentEtcdServer != null) {
+            return Collections.singleton(currentEtcdServer.getClusterURI());
+        } else {
+            throw new IllegalStateException("getClusterURIs() can only be called with active rule");
+        }
+    }
+}
